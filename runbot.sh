5500bbd4f5e180b3d964dc17b8fb9033a45bead4
wawreszukd <wawreszukd@gmail.com>
2025-07-15 09:45:36 +0200

    fixed runbot

--- runbot.sh
+++ runbot.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if pgrep "runbot.sh" > /dev/null
+if pgrep "deno" > /dev/null
 then
     echo "zabijam poprzedni proces"
-    pkill -f runbot.sh
+    pkill -f deno
 else
     echo "Uruchamiam bota..."
     nohup deno --allow-read --allow-write --allow-env --env-file --alow-net index.ts > logs.txt 2>&1 &
 
-    echo "Nowy proces Node.js został uruchomiony w tle."
+    echo "Nowy proces bota został uruchomiony w tle."
     echo "Logi są zapisywane do pliku logs.txt."
 fi
 

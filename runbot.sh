406730f2e25b0e679743d4c399093ae6fd7b398d
wawreszukd <wawreszukd@gmail.com>
2025-07-15 09:50:03 +0200

    fixed runbot

--- runbot.sh
+++ runbot.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-if pgrep "deno" > /dev/null
-then
-    echo "zabijam poprzedni proces"
-    pkill -f deno
-else
-    echo "Uruchamiam bota..."
-    nohup deno --allow-read --allow-write --allow-env --env-file --alow-net index.ts > logs.txt 2>&1 &
 
-    echo "Nowy proces bota został uruchomiony w tle."
-    echo "Logi są zapisywane do pliku logs.txt."
+
+if pgrep -f "deno run" > /dev/null
+then
+    echo "Zabijam poprzedni proces Deno..."
+    pkill -f "deno run"
+
+    sleep 2
+else
+    echo "Nie znaleziono aktywnego procesu Deno."
 fi
 
 
+echo "Uruchamiam nowego bota..."
+
+nohup deno run --allow-read --allow-write --allow-env --env-file --allow-net index.ts > logs.txt 2>&1 &
+
+echo "Nowy proces bota został uruchomiony w tle."
+echo "Logi są zapisywane do pliku logs.txt."
